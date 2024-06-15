01b07edf70fbcd75892115cd8a32d6bc8ef7b708
Egorke Ovie <egorkeovie@gmail.com>
2024-06-15 08:05:22 +0100

    routes

--- src/Components/Display.jsx
+++ src/Components/Display.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import Login from "./Login";
 import SignUp from "./SignUp";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 const Display = () => {
-    return ( 
-        <div className="display">
-            <Login />
-            <SignUp />
-        </div>
-     );
-}
- 
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
+
 export default Display;
