9fdd3ee924c8c1cbbc284ca0e76ec0d91929f125
Egorke Ovie <egorkeovie@gmail.com>
2024-06-29 18:06:09 +0100

    background

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,62 +1,70 @@
-import React from 'react';
-import { Background2, DisplayImage } from '../assets';
-import './style.css';
+import React from "react";
+import { DisplayImage } from "../assets";
+import "./style.css";
 
 const Login = () => {
   return (
     <React.Fragment>
       <div className="login">
-      <div className="row">
-        {/* Left Column with Display Image */}
-        <div className="col-sm-12 col-md-12 col-lg-5 text-center specta" style={{ backgroundColor: '#2d85de', maxWidth: '100%', maxHeight: '100vh'}}>
-          <img src={ DisplayImage } alt="Display" className='sign-img' />
-        </div>
-        
-        {/* Right Column with Login Form */}
-        <div className="col-sm-12 col-md-12 col-lg-7 ajaz" style={{ backgroundImage: `url(${Background2})`, backgroundRepeat: 'no-repeat', maxHeight: '100vh', backgroundSize: 'cover', backgroundPosition: 'center center' }}>
-            <h2 className='mt-3 text-center'>Login</h2>
-          <div className="forms">
-            <form>
-              {/* Email Input Field */}
-              <div className="form-group">
-                <label htmlFor="exampleInputEmail1">Email address</label>
-                <input
-                  type="email"
-                  name='email'
-                  className="form-control input mb-3"
-                  id="exampleInputEmail1"
-                  placeholder='Enter your email'
-                />
-              </div>
-              
-              {/* Password Input Field */}
-              <div className="form-group">
-                <label htmlFor="exampleInputPassword1">Password</label>
-                <input
-                  type="password"
-                  name='password'
-                  className="form-control input mb-3"
-                  id="exampleInputPassword1"
-                  placeholder='Enter your password'
-                />
-              </div>
-            </form>
-            
-            <center>
-            {/* Login Button with Spinner */}
-            <button className='button button-blue' style={{textAlign: 'center'}}>
-              <div className="spinner-border spinner-border-sm text-light spin" role='status'>
-                <span className='sr-only'></span>
-              </div>
-              Login
-            </button>
-            </center>
+        <div className="row">
+          {/* Left Column with Display Image */}
+          <div
+            className="col-sm-12 col-md-12 col-lg-5 text-center specta">
+            <img src={DisplayImage} alt="Display" className="sign-img" />
+          </div>
+
+          {/* Right Column with Login Form */}
+          <div
+            className="col-sm-12 col-md-12 col-lg-7 ajaz">
+            <h2 className="mt-3 text-center">Login</h2>
+            <div className="forms">
+              <form>
+                {/* Email Input Field */}
+                <div className="form-group">
+                  <label htmlFor="exampleInputEmail1">Email address</label>
+                  <input
+                    type="email"
+                    name="email"
+                    className="form-control input mb-3"
+                    id="exampleInputEmail1"
+                    placeholder="Enter your email"
+                  />
+                </div>
+
+                {/* Password Input Field */}
+                <div className="form-group">
+                  <label htmlFor="exampleInputPassword1">Password</label>
+                  <input
+                    type="password"
+                    name="password"
+                    className="form-control input mb-3"
+                    id="exampleInputPassword1"
+                    placeholder="Enter your password"
+                  />
+                </div>
+              </form>
+
+              <center>
+                {/* Login Button with Spinner */}
+                <button
+                  className="button button-blue"
+                  style={{ textAlign: "center" }}
+                >
+                  <div
+                    className="spinner-border spinner-border-sm text-light spin"
+                    role="status"
+                  >
+                    <span className="sr-only"></span>
+                  </div>
+                  Login
+                </button>
+              </center>
+            </div>
           </div>
         </div>
       </div>
-      </div>
     </React.Fragment>
   );
-}
+};
 
 export default Login;
