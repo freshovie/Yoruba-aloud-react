d9167292061f5863779ef3c8697e62eb73c8a1c8
Egorke Ovie <egorkeovie@gmail.com>
2024-07-12 22:38:16 +0100

    add on

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -14,7 +14,7 @@ const Dashboard = () => {
       <Sidebar onButtonClick={handleButtonClick} activeContent={activeContent} />
       <div className="content">
         {activeContent === 'dashboard' && <div className="dashboard-content"><h1>Dashboard</h1></div>}
-        {activeContent === 'categories' && <div className="categories-content"><h1>Categories</h1></div>}
+        {activeContent === 'categories' && <div className="categories-content"><h1>Categories love</h1></div>}
         {activeContent === 'learn-material' && <div className="learn-material-content"><h1>Learning Materials</h1></div>}
         {activeContent === 'quiz' && <div className="quiz-content"><h1>Quiz</h1></div>}
         {activeContent === 'adminprofile' && <div className="admin-profile-content"><h1>Admin Profile</h1></div>}
